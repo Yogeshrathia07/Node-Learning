93f1199e3ed27d89ff336ff9c33168e78879b510
Yogeshrathia07 <yogesh.rathia07@gmail.com>
2025-04-12 10:57:55 +0530

    mini project 1

---
+++ s_17/app.js
@@ -0,0 +1,95 @@
+const express= require('express');
+const app=express();
+
+const path=require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+const userModel=require('./models/user');
+const postModel=require('./models/post');
+const cookieParser = require('cookie-parser');
+
+app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+app.get('/login',(req,res)=>{
+    res.render('login');
+});
+
+app.post('/register',async (req,res)=>{
+    let {name,email,password,username,age}=req.body;
+    // res.render('register');
+    let user=await userModel.findOne({email});
+    if(user){
+        return res.status(400).send({message:'User already exists'});
+    }
+
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(password,salt,async (err,hash)=>{
+            let user=await userModel.create({
+                username,
+                email,
+                password:hash,
+                age,
+                name
+            })
+            let token=jwt.sign({email:email,userid: user._id},"abcd");
+            res.cookie('token',token,{httpOnly:true});
+            res.status(200).send({message:'User registered successfully'});
+        })
+
+    })
+
+});
+app.post('/login',async (req,res)=>{
+    let {email,password}=req.body;
+    // res.render('register');
+    let user=await userModel.findOne({email});
+    if(!user){
+        return res.status(400).send("Something went wrong!");
+    }
+
+    bcrypt.compare(password,user.password,(err,result)=>{
+        if(result){
+            let token=jwt.sign({email:email,userid: user._id},"abcd");
+            res.cookie('token',token);
+            res.status(200).send('User logged in successfully');
+        }
+        else res.status(400).send('Invalid credentials');
+    })
+
+});
+
+app.get('/logout',(req,res)=>{
+    res.clearCookie('token');
+    res.redirect('/login');
+})
+
+
+app.get('/profile',isloggedin,(req,res)=>{
+    console.log(req.user);
+    res.render('login');
+})
+function isloggedin(req,res,next){
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).send("You are not logged in! Token missing.");
+    }
+    else
+    {
+        let data=jwt.verify(token,"abcd");
+        req.user=data;
+    }
+    next();
+}
+
+app.listen(3000,()=>{
+    console.log('Server is running on http://localhost:3000');
+});
